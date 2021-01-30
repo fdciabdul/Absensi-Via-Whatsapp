efcbd4c96157fa137bafc3fe699bcffb155e0c51
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2021-01-31 02:56:00 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,101 @@
+const qrcode = require("qrcode-terminal");
+const moment = require("moment");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const dl = require("./lib/downloadImage.js");
+const fetch = require('node-fetch');
+const urlencode = require("urlencode");
+const axios = require("axios");
+const imageToBase64 = require('image-to-base64');
+const menu = require("./menu.js");
+const calc = require("./lib/calc.js");
+const { exec } = require("child_process");
+const
+{
+   WAConnection,
+   MessageType,
+   Presence,
+   MessageOptions,
+   Mimetype,
+   WALocationMessage,
+   WA_MESSAGE_STUB_TYPES,
+   ReconnectMode,
+   ProxyAgent,
+   waChatKey,
+} = require("@adiwajshing/baileys");
+var jam = moment().format("HH:mm");
+
+
+const conn = new WAConnection()
+conn.on('qr', qr =>
+{
+   qrcode.generate(qr,
+   {
+      small: true
+   });
+   console.log(`[ ${moment().format("HH:mm:ss")} ] Please Scan QR with app!`);
+});
+
+conn.on('credentials-updated', () =>
+{
+   // save credentials whenever updated
+   console.log(`credentials updated!`)
+   const authInfo = conn.base64EncodedAuthInfo() // get all the auth info we need to restore this session
+   fs.writeFileSync('./session.json', JSON.stringify(authInfo, null, '\t')) // save this info to a file
+})
+fs.existsSync('./session.json') && conn.loadAuthInfo('./session.json')
+// uncomment the following line to proxy the connection; some random proxy I got off of: https://proxyscrape.com/free-proxy-list
+//conn.connectOptions.agent = ProxyAgent ('http://1.0.180.120:8080')
+conn.connect();
+
+conn.on('user-presence-update', json => console.log(json.id + ' presence is ' + json.type))
+conn.on('message-status-update', json =>
+{
+   const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
+   console.log(`${json.to}${participant} acknlowledged message(s) ${json.ids} as ${json.type}`)
+})
+
+conn.on('message-new', async(m) =>
+{
+   const messageContent = m.message
+   const text = m.message.conversation
+   let id = m.key.remoteJid
+   const messageType = Object.keys(messageContent)[0] // message will always contain one key signifying what kind of message
+   let imageMessage = m.message.imageMessage;
+   console.log(`[ ${moment().format("HH:mm:ss")} ] (${id.split("@s.whatsapp.net")[0]} => ${text}`);
+
+
+// Groups
+
+if (text.includes("!buatgrup"))
+   {
+var nama = text.split("!buatgrup")[1].split("-nomor")[0];
+var nom = text.split("-nomor")[1];
+var numArray = nom.split(",");
+for ( var i = 0; i < numArray.length; i++ ) {
+    numArray[i] = numArray[i] +"@s.whatsapp.net";
+}
+var str = numArray.join("");
+console.log(str)
+const group = await conn.groupCreate (nama, str)
+console.log ("created group with id: " + group.gid)
+conn.sendMessage(group.gid, "hello everyone", MessageType.extendedText) // say hello to everyone on the group
+
+}
+
+// FF
+if (messageType === MessageType.location || messageType === MessageType.liveLocation) {
+   const locMessage = m.message[messageType];
+  
+   console.log(`${id} sent location (lat: ${locMessage.degreesLatitude}, long: ${locMessage.degreesLongitude})`)
+   var meter = calc.get(locMessage.degreesLatitude,locMessage.degreesLongitude,"-6.5646795,","106.6779");
+  console.log(meter);
+   conn.sendMessage(id, meter.toFixed(2), MessageType.text)
+   if (messageType === MessageType.liveLocation) {
+      conn.sendMessage(id,`${id} sent live location for duration: ${m.duration/60}`, MessageType.text)
+   }
+}
+
+  
+
+})
